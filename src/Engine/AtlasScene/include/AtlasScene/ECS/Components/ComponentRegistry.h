a25f17ed85e7d9a3b05d19945c94312e0f434d4e
Carl <slxxls92@gmail.com>
2022-09-29 23:08:38 +0100

    Component registry now stores unique IDs for component types and fields

--- src/Engine/AtlasScene/include/AtlasScene/ECS/Components/ComponentRegistry.h
+++ src/Engine/AtlasScene/include/AtlasScene/ECS/Components/ComponentRegistry.h
@@ -10,23 +10,43 @@
 
 namespace atlas::scene
 {
+    DEFINE_ID_CONTAINER(ComponentInfoId, int32_t);
+    DEFINE_ID_CONTAINER(ComponentFieldInfoId, int32_t);
+
     class ComponentRegistry
     {
     public:
-        struct ComponentField
+        struct ComponentFieldInfo
         {
             std::string_view m_Name;
             std::string_view m_Type;
             std::size_t m_Offset;
             std::size_t m_Size;
+            ComponentFieldInfoId m_FieldId;
         };
 
-        struct ComponentRegistration
+        struct ComponentInfo
         {
-            int32_t m_UniqueId;
+            ComponentInfoId m_UniqueId;
             std::string_view m_ComponentName;
 
-            std::vector<ComponentField> m_Fields;
+            std::vector<ComponentFieldInfo> m_Fields;
+
+            const ComponentFieldInfo& GetFieldInfo(const ComponentFieldInfoId& fieldId) const
+            {
+                assert(fieldId.IsValid() && fieldId.m_Value < m_Fields.size());
+                return m_Fields[fieldId.m_Value];
+            }
+
+#if defined(DEBUG)
+            void ValidateFieldOffsets()
+            {
+                for(int32_t i = 0; i < (int32_t)m_Fields.size(); i++)
+                {
+                    assert(i == m_Fields[i].m_FieldId);
+                }
+            }
+#endif
         };
 
         using PoolFactory = PoolBase* (*)(void);
@@ -38,12 +58,24 @@ namespace atlas::scene
         ComponentRegistry operator=(const ComponentRegistry&) = delete;
         ComponentRegistry operator=(const ComponentRegistry&&) = delete;
 
+        static const ComponentInfo& GetComponentInfo(const ComponentInfoId& componentId)
+        {
+            assert(componentId.IsValid() && componentId.m_Value < m_ComponentRegistrations.size());
+            return m_ComponentRegistrations[componentId.m_Value];
+        }
+
         template <typename TComponent>
-        static void RegisterComponent(ComponentRegistration&& registration)
+        static void RegisterComponent(ComponentInfo&& registration)
         {
             std::lock_guard lock{m_ComponentRegistrationMutex};
-            registration.m_UniqueId = core::TemplatedUniquenessCounter<TComponent, ComponentRegistry>::Ensure();
+            registration.m_UniqueId = ComponentInfoId { core::TemplatedUniquenessCounter<TComponent, ComponentRegistry>::Ensure() };
 
+#if defined(DEBUG)
+            registration.ValidateFieldOffsets();
+#endif
+
+            // Check out assumption that the UniqueId maps with the registration index
+            assert(m_ComponentRegistrations.size() == registration.m_UniqueId.m_Value);
             m_ComponentRegistrations.emplace_back(registration);
             m_ComponentPoolFactory.push_back([] { return static_cast<PoolBase*>(new ComponentPool<TComponent>()); });
         }
@@ -60,11 +92,21 @@ namespace atlas::scene
         }
 
         template <typename TComponent>
+        static ComponentInfoId GetComponentId()
+        {
+            return ComponentInfoId { core::TemplatedUniquenessCounter<TComponent, ComponentRegistry>::GetTypeValue() };
+        }
+
+        template <typename TComponent>
         static uint64_t GetComponentMask()
         {
-            const uint64_t index = core::TemplatedUniquenessCounter<TComponent, ComponentRegistry>::GetTypeValue();
-            assert(index != -1); // Component has not been registered
-            return 1ULL << index;
+            return GetComponentMask(GetComponentId<TComponent>());
+        }
+
+        static uint64_t GetComponentMask(const ComponentInfoId componentId)
+        {
+            assert(componentId.IsValid()); // Component has not been registered
+            return 1ULL << componentId.m_Value;
         }
 
         static std::mutex& GetComponentRegistrationMutex()
@@ -72,14 +114,14 @@ namespace atlas::scene
             return m_ComponentRegistrationMutex;
         }
 
-        static const std::vector<ComponentRegistration>& GetComponentRegistrations()
+        static const std::vector<ComponentInfo>& GetComponentRegistrations()
         {
             return m_ComponentRegistrations;
         }
 
     private:
         inline static std::mutex m_ComponentRegistrationMutex;
-        inline static std::vector<ComponentRegistration> m_ComponentRegistrations;
+        inline static std::vector<ComponentInfo> m_ComponentRegistrations;
         inline static std::vector<PoolFactory> m_ComponentPoolFactory;
     };
 }
