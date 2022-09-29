86e0a826f1bdf1c71bc958db58eefbd151ece0b1
Carl <slxxls92@gmail.com>
2022-09-29 23:08:10 +0100

    Added general purpose ID containers

---
+++ src/Engine/AtlasCore/include/AtlasCore/IdContainer.h
@@ -0,0 +1,45 @@
+﻿#pragma once
+#include <cstdint>
+
+namespace atlas::core
+{
+    template<typename TSelf, typename TValueType = int32_t>
+    struct IdContainer
+    {
+        static constexpr int c_invalidValue = -1;
+
+        IdContainer()
+        {
+        }
+
+        explicit IdContainer(const TValueType value)
+            : m_Value(value)
+        {
+        }
+
+        static TSelf Invalid() { return TSelf{c_invalidValue}; }
+
+        [[nodiscard]] bool IsInvalid() const { return m_Value == c_invalidValue; }
+
+        bool operator==(const IdContainer<TSelf, TValueType> other) const
+        {
+            return m_Value == other.m_Value;
+        }
+
+        bool operator<(const IdContainer<TSelf, TValueType> other) const
+        {
+            return m_Value < other.m_Value;
+        }
+
+        [[nodiscard]] bool IsValid() const { return m_Value >= 0; }
+
+        TValueType m_Value = c_invalidValue;
+    };
+
+#define DEFINE_ID_CONTAINER(TYPE, VALUETYPE)\
+    struct TYPE : atlas::core::IdContainer<TYPE, VALUETYPE>\
+    {\
+        TYPE() {}\
+        explicit TYPE(const VALUETYPE value) : IdContainer(value) {}\
+    }
+}
