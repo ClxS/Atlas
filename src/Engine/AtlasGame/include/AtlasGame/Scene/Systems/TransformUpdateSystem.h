8b65a63d3b06cca4d25b0a71773c2c249b2b6d87
Carl <slxxls92@gmail.com>
2022-10-14 19:26:57 +0100

    Added basic transform update system

---
+++ src/Engine/AtlasGame/include/AtlasGame/Scene/Systems/TransformUpdateSystem.h
@@ -0,0 +1,13 @@
+﻿#pragma once
+#include "AtlasScene/ECS/Systems/SystemBase.h"
+
+namespace atlas::game::scene::systems
+{
+    class TransformUpdateSystem final : public atlas::scene::SystemBase
+    {
+    public:
+        [[nodiscard]] std::string_view GetName() const override { return "TransformUpdateSystem"; }
+
+        void Update(atlas::scene::EcsManager& ecs) override;
+    };
+}
---
+++ src/Engine/AtlasGame/src/Scene/Systems/TransformUpdateSystem.cpp
@@ -0,0 +1,15 @@
+﻿#include "AtlasGamePCH.h"
+#include "TransformUpdateSystem.h"
+
+#include "TransformComponent.h"
+#include "TransformPrivateComponent.h"
+#include "AtlasScene/ECS/Components/EcsManager.h"
+
+void atlas::game::scene::systems::TransformUpdateSystem::Update(atlas::scene::EcsManager& ecs)
+{
+    for(auto [entity, transform, transformPrivate] : ecs.IterateEntityComponents<components::TransformComponent, components::TransformPrivateComponent>())
+    {
+        Eigen::Affine3f t{Eigen::Translation3f(transform.m_Position.cast<float>())};
+        transformPrivate.m_Transform = (transform.m_Orientation * t).matrix();
+    }
+}
