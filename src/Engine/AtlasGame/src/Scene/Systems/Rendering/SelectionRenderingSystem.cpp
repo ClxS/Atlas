7cd5b72f9533afb7bcaee490a75944356af6bb29
Carl <slxxls92@gmail.com>
2022-10-21 19:54:39 +0100

    Cleanup + Selection now renders a debug bounding sphere

--- src/Engine/AtlasGame/src/Scene/Systems/Rendering/SelectionRenderingSystem.cpp
+++ src/Engine/AtlasGame/src/Scene/Systems/Rendering/SelectionRenderingSystem.cpp
@@ -5,83 +5,90 @@
 #include "ModelRenderSystem.h"
 #include "PostProcessSystem.h"
 #include "SelectionComponent.h"
+#include "TransformComponent.h"
 #include "TransformPrivateComponent.h"
 
 #include "AtlasAppHost/Application.h"
 #include "AtlasRender/AssetRegistry.h"
 #include "AtlasRender/Renderer.h"
+#include "AtlasRender/Debug/debugdraw.h"
 #include "AtlasResource/ResourceLoader.h"
 #include "AtlasScene/ECS/Components/EcsManager.h"
 
+using namespace atlas::game::scene::systems::rendering;
+using namespace atlas::game::components;
+using namespace interaction;
+using namespace rendering;
+using namespace atlas::render::resources::core_bundle;
+using namespace bgfx;
+
 namespace
 {
-    struct VertexLayout
+    namespace local
     {
-        Eigen::Vector3f m_Position;
-        Eigen::Vector2f m_Uv;
-    };
+        struct VertexLayout
+        {
+            Eigen::Vector3f m_Position;
+            Eigen::Vector2f m_Uv;
+        };
+    }
 }
 
-void atlas::game::scene::systems::rendering::SelectionRenderingSystem::Initialise(atlas::scene::EcsManager& ecsManager)
+void SelectionRenderingSystem::Initialise(atlas::scene::EcsManager& ecsManager)
 {
+    using namespace resource;
+    using namespace render;
+    using namespace resources;
+
     auto [width, height] = app_host::Application::Get().GetAppDimensions();
     m_Buffers.m_Silhouette.Initialise(width, height, true, true);
     m_Buffers.m_BlurredSilhouette.Initialise(width / 4, height / 4, false, false);
 
-    bgfx::setViewName(m_SilhoutteView, "SelectionStencil");
-    bgfx::setViewName(m_BlurredView, "SelectionBlur");
-    bgfx::setViewName(m_OutlineBlitView + 0, "SelectionBlit0");
-    bgfx::setViewName(m_OutlineBlitView + 1, "SelectionBlit1");
+    setViewName(m_SilhouetteView, "Selection_Stencil");
+    setViewName(m_BlurredView, "Selection_Blur");
+    setViewName(m_OutlineFormationView, "Selection_FormOutline");
+    setViewName(m_OutlineBlitView, "Selection_BlitOutline");
 
-    bgfx::setViewRect(m_SilhoutteView, 0, 0, static_cast<uint16_t>(width), static_cast<uint16_t>(height));
-    bgfx::setViewClear(m_SilhoutteView, BGFX_CLEAR_COLOR | BGFX_CLEAR_DEPTH | BGFX_CLEAR_STENCIL, 0x00000000, 1.0f, 0);
+    setViewRect(m_SilhouetteView, 0, 0, static_cast<uint16_t>(width), static_cast<uint16_t>(height));
+    setViewClear(m_SilhouetteView, BGFX_CLEAR_COLOR | BGFX_CLEAR_DEPTH | BGFX_CLEAR_STENCIL, 0x00000000, 1.0f, 0);
 
-    bgfx::setViewClear(m_OutlineBlitView + 0, BGFX_CLEAR_COLOR, 0x00000000);
-    setViewMode(m_OutlineBlitView + 0, bgfx::ViewMode::Sequential);
-    setViewRect(m_OutlineBlitView + 0, 0, 0, bgfx::BackbufferRatio::Equal);
+    setViewClear(m_OutlineFormationView, BGFX_CLEAR_COLOR, 0x00000000);
+    setViewMode(m_OutlineFormationView, ViewMode::Sequential);
+    setViewRect(m_OutlineFormationView, 0, 0, BackbufferRatio::Equal);
 
-    bgfx::setViewClear(m_OutlineBlitView + 1, BGFX_CLEAR_NONE);
-    setViewMode(m_OutlineBlitView + 1, bgfx::ViewMode::Sequential);
-    setViewRect(m_OutlineBlitView + 1, 0, 0, bgfx::BackbufferRatio::Equal);
+    setViewClear(m_OutlineBlitView, BGFX_CLEAR_NONE);
+    setViewMode(m_OutlineBlitView, ViewMode::Sequential);
+    setViewRect(m_OutlineBlitView, 0, 0, BackbufferRatio::Equal);
 
-    bgfx::setViewClear(m_BlurredView, BGFX_CLEAR_NONE);
-    setViewMode(m_BlurredView, bgfx::ViewMode::Sequential);
-    setViewRect(m_BlurredView, 0, 0, bgfx::BackbufferRatio::Equal);
+    setViewClear(m_BlurredView, BGFX_CLEAR_NONE);
+    setViewMode(m_BlurredView, ViewMode::Sequential);
+    setViewRect(m_BlurredView, 0, 0, BackbufferRatio::Equal);
 
-    setViewFrameBuffer(m_SilhoutteView, m_Buffers.m_Silhouette.GetHandle());
+    setViewFrameBuffer(m_SilhouetteView, m_Buffers.m_Silhouette.GetHandle());
     setViewFrameBuffer(m_BlurredView, m_Buffers.m_BlurredSilhouette.GetHandle());
-    setViewFrameBuffer(m_OutlineBlitView + 0, m_Buffers.m_Silhouette.GetHandle());
-    setViewFrameBuffer(m_OutlineBlitView + 1, m_TargetFrameBuffer);
+    setViewFrameBuffer(m_OutlineFormationView, m_Buffers.m_Silhouette.GetHandle());
+    setViewFrameBuffer(m_OutlineBlitView, m_TargetFrameBuffer);
 
-    m_Programs.m_StencilUpdate = resource::ResourceLoader::LoadAsset<render::resources::CoreBundle,
-                                                                render::ShaderProgram>(
-        render::resources::core_bundle::shaders::outline::c_outline_stencil);
+    m_Programs.m_StencilUpdate = ResourceLoader::LoadAsset<CoreBundle, ShaderProgram>(shaders::outline::c_outline_stencil);
 
-    m_Programs.m_CreateBlur = resource::ResourceLoader::LoadAsset<render::resources::CoreBundle,
-                                                                render::ShaderProgram>(
-        render::resources::core_bundle::shaders::outline::c_outline_blur);
-    m_Programs.m_Copy = resource::ResourceLoader::LoadAsset<render::resources::CoreBundle,
-                                                            render::ShaderProgram>(
-                                                            render::resources::core_bundle::shaders::postprocess::c_copy);
-    m_Programs.m_Clear = resource::ResourceLoader::LoadAsset<render::resources::CoreBundle,
-                                                            render::ShaderProgram>(
-        render::resources::core_bundle::shaders::outline::c_outline_clear);
+    m_Programs.m_CreateBlur = ResourceLoader::LoadAsset<CoreBundle, ShaderProgram>(shaders::outline::c_outline_blur);
+    m_Programs.m_Copy = ResourceLoader::LoadAsset<CoreBundle, ShaderProgram>(shaders::postprocess::c_copy);
+    m_Programs.m_Clear = ResourceLoader::LoadAsset<CoreBundle, ShaderProgram>(shaders::outline::c_outline_clear);
 
-    m_Samplers.m_Color = createUniform("s_texColor", bgfx::UniformType::Sampler);
-
-    static_assert(sizeof(VertexLayout) == sizeof(float) * 3 + sizeof(float) * 2);
+    m_Samplers.m_Color = createUniform("s_texColor", UniformType::Sampler);
+    static_assert(sizeof(local::VertexLayout) == sizeof(float) * 3 + sizeof(float) * 2);
     m_OutlineVertexLayout
         .begin()
-        .add(bgfx::Attrib::Position, 3, bgfx::AttribType::Float)
-        .add(bgfx::Attrib::TexCoord0, 2, bgfx::AttribType::Float)
+        .add(Attrib::Position, 3, AttribType::Float)
+        .add(Attrib::TexCoord0, 2, AttribType::Float)
         .end();
 
-    const float screenSpaceWidth = 1.0f;
-    const float screenSpaceHeight = 1.0f;
-    const float minX = -screenSpaceWidth;
-    const float maxX = screenSpaceWidth;
-    const float minY = -screenSpaceHeight;
-    const float maxy = screenSpaceHeight;
+    constexpr float screenSpaceWidth = 1.0f;
+    constexpr float screenSpaceHeight = 1.0f;
+    constexpr float minX = -screenSpaceWidth;
+    constexpr float maxX = screenSpaceWidth;
+    constexpr float minY = -screenSpaceHeight;
+    constexpr float maxy = screenSpaceHeight;
     constexpr float z = 0.0f;
 
     constexpr float minU = 0.0f;
@@ -89,12 +96,12 @@ void atlas::game::scene::systems::rendering::SelectionRenderingSystem::Initialis
     float minV = 0.0f;
     float maxV = 1.0f;
 
-    if (!bgfx::getCaps()->originBottomLeft)
+    if (!getCaps()->originBottomLeft)
     {
         std::swap(minV, maxV);
     }
 
-    VertexLayout vertices[6];
+    local::VertexLayout vertices[6];
     vertices[0].m_Position = { minX, minY, z };
     vertices[1].m_Position = { maxX, minY, z };
     vertices[2].m_Position = { maxX, maxy, z };
@@ -109,22 +116,22 @@ void atlas::game::scene::systems::rendering::SelectionRenderingSystem::Initialis
     vertices[4].m_Uv = { maxU, maxV };
     vertices[5].m_Uv = { minU, maxV };
 
-    static_assert(sizeof(vertices) == sizeof(VertexLayout) * 6);
-    const bgfx::Memory* vertexMemory = bgfx::alloc(m_OutlineVertexLayout.getSize(6));
+    static_assert(sizeof vertices == sizeof(local::VertexLayout) * 6);
+    const Memory* vertexMemory = alloc(m_OutlineVertexLayout.getSize(6));
     std::memcpy(vertexMemory->data, vertices, m_OutlineVertexLayout.getSize(6));
     m_FullScreenQuad = createVertexBuffer(vertexMemory, m_OutlineVertexLayout);
 }
 
-void atlas::game::scene::systems::rendering::SelectionRenderingSystem::Render(atlas::scene::EcsManager& ecs)
+void SelectionRenderingSystem::Render(atlas::scene::EcsManager& ecs)
 {
     auto [width, height] = app_host::Application::Get().GetAppDimensions();
     m_Buffers.m_Silhouette.EnsureSize(width, height);
     m_Buffers.m_BlurredSilhouette.EnsureSize(width / 4, height / 4);
 
-    bgfx::setViewRect(m_SilhoutteView, 0, 0, static_cast<uint16_t>(width), static_cast<uint16_t>(height));
-    bgfx::setViewRect(m_OutlineBlitView + 0, 0, 0, static_cast<uint16_t>(width), static_cast<uint16_t>(height));
-    bgfx::setViewRect(m_OutlineBlitView + 1, 0, 0, static_cast<uint16_t>(width), static_cast<uint16_t>(height));
-    bgfx::touch(m_SilhoutteView);
+    setViewRect(m_SilhouetteView, 0, 0, static_cast<uint16_t>(width), static_cast<uint16_t>(height));
+    setViewRect(m_OutlineFormationView, 0, 0, static_cast<uint16_t>(width), static_cast<uint16_t>(height));
+    setViewRect(m_OutlineBlitView, 0, 0, static_cast<uint16_t>(width), static_cast<uint16_t>(height));
+    touch(m_SilhouetteView);
     RenderSelectedObjects(
         ecs,
         1);
@@ -132,75 +139,86 @@ void atlas::game::scene::systems::rendering::SelectionRenderingSystem::Render(at
     ApplyOutline();
 }
 
-void atlas::game::scene::systems::rendering::SelectionRenderingSystem::RenderOutline() const
+void SelectionRenderingSystem::RenderOutline() const
 {
     setVertexBuffer(0, m_FullScreenQuad.Get());
-    bgfx::setState(BGFX_STATE_WRITE_RGB | BGFX_STATE_WRITE_A | BGFX_STATE_DEPTH_TEST_ALWAYS);
+    setState(BGFX_STATE_WRITE_RGB | BGFX_STATE_WRITE_A | BGFX_STATE_DEPTH_TEST_ALWAYS);
     setTexture(0, m_Samplers.m_Color.Get(), getTexture(m_Buffers.m_Silhouette.GetHandle()));
     submit(m_BlurredView, m_Programs.m_CreateBlur->GetHandle());
 
     setVertexBuffer(0, m_FullScreenQuad.Get());
-    bgfx::setState(BGFX_STATE_WRITE_RGB | BGFX_STATE_WRITE_A | BGFX_STATE_DEPTH_TEST_ALWAYS);
-    bgfx::setStencil(BGFX_STENCIL_TEST_EQUAL
-        | BGFX_STENCIL_FUNC_REF(0)
-        | BGFX_STENCIL_FUNC_RMASK(0xff)
-        | BGFX_STENCIL_OP_FAIL_S_KEEP
-        | BGFX_STENCIL_OP_FAIL_Z_KEEP
-        | BGFX_STENCIL_OP_PASS_Z_KEEP, BGFX_STENCIL_NONE);
+    setState(BGFX_STATE_WRITE_RGB | BGFX_STATE_WRITE_A | BGFX_STATE_DEPTH_TEST_ALWAYS);
+    setStencil(BGFX_STENCIL_TEST_EQUAL
+               | BGFX_STENCIL_FUNC_REF(0)
+               | BGFX_STENCIL_FUNC_RMASK(0xff)
+               | BGFX_STENCIL_OP_FAIL_S_KEEP
+               | BGFX_STENCIL_OP_FAIL_Z_KEEP
+               | BGFX_STENCIL_OP_PASS_Z_KEEP, BGFX_STENCIL_NONE);
     setTexture(0, m_Samplers.m_Color.Get(), getTexture(m_Buffers.m_BlurredSilhouette.GetHandle()));
-    submit(m_OutlineBlitView + 0, m_Programs.m_CreateBlur->GetHandle());
+    submit(m_OutlineFormationView, m_Programs.m_CreateBlur->GetHandle());
 }
 
-void atlas::game::scene::systems::rendering::SelectionRenderingSystem::ApplyOutline()
+void SelectionRenderingSystem::ApplyOutline()
 {
     setVertexBuffer(0, m_FullScreenQuad.Get());
-    bgfx::setState(BGFX_STATE_WRITE_RGB | BGFX_STATE_DEPTH_TEST_ALWAYS | BGFX_STATE_BLEND_FUNC(BGFX_STATE_BLEND_SRC_ALPHA, BGFX_STATE_BLEND_INV_SRC_ALPHA));
+    setState(BGFX_STATE_WRITE_RGB | BGFX_STATE_DEPTH_TEST_ALWAYS | BGFX_STATE_BLEND_FUNC(BGFX_STATE_BLEND_SRC_ALPHA, BGFX_STATE_BLEND_INV_SRC_ALPHA));
     setTexture(0, m_Samplers.m_Color.Get(), getTexture(m_Buffers.m_Silhouette.GetHandle()));
-    submit(m_OutlineBlitView + 1, m_Programs.m_Copy->GetHandle());
+    submit(m_OutlineBlitView, m_Programs.m_Copy->GetHandle());
 }
 
-void atlas::game::scene::systems::rendering::SelectionRenderingSystem::RenderSelectedObjects(
+void SelectionRenderingSystem::RenderSelectedObjects(
     atlas::scene::EcsManager& ecs,
     const uint8_t stencilValue) const
 {
-    for(auto [entity, model, position, _] : ecs.IterateEntityComponents<
-        components::rendering::ModelComponent,
-        components::TransformPrivateComponent,
-        components::interaction::SelectionComponent>())
+    using namespace render::debug;
+
+    for(auto [entity, model, positionRaw, position, _] : ecs.IterateEntityComponents<
+            ModelComponent,
+            TransformComponent,
+            TransformPrivateComponent,
+            SelectionComponent>())
     {
-        if (!model.m_Model || !model.m_Model->GetMesh() || !model.m_Model->GetProgram())
+        if (!model.m_Model || !model.m_Model->GetMesh())
         {
             continue;
         }
 
-        assert(0 != (BGFX_CAPS_INSTANCING & bgfx::getCaps()->supported));
-        constexpr uint16_t instanceStride = sizeof(Eigen::Matrix4f);
-        constexpr uint32_t totalPositions = 1;
-        const uint32_t numDrawableInstances = bgfx::getAvailInstanceDataBuffer(totalPositions, instanceStride);
-
-        bgfx::InstanceDataBuffer idb{};
-        allocInstanceDataBuffer(&idb, numDrawableInstances, instanceStride);
-
-        bgfx::setState(
-            BGFX_STATE_WRITE_RGB
-            | BGFX_STATE_WRITE_A
-            | BGFX_STATE_WRITE_Z
-            | BGFX_STATE_DEPTH_TEST_ALWAYS
-            | BGFX_STATE_CULL_CW);
-        bgfx::setStencil(
-            BGFX_STENCIL_TEST_ALWAYS
-            | BGFX_STENCIL_FUNC_REF(stencilValue)
-            | BGFX_STENCIL_FUNC_RMASK(0xff)
-            | BGFX_STENCIL_OP_FAIL_S_REPLACE
-            | BGFX_STENCIL_OP_FAIL_Z_REPLACE
-            | BGFX_STENCIL_OP_PASS_Z_REPLACE,
-            BGFX_STENCIL_NONE);
-
         drawInstanced(
-                m_SilhoutteView,
+                m_SilhouetteView,
                 model.m_Model,
                 m_Programs.m_StencilUpdate,
                 { position.m_Transform },
+                BGFX_STATE_WRITE_RGB
+                    | BGFX_STATE_WRITE_A
+                    | BGFX_STATE_WRITE_Z
+                    | BGFX_STATE_DEPTH_TEST_ALWAYS
+                    | BGFX_STATE_CULL_CW,
+                BGFX_STENCIL_TEST_ALWAYS
+                    | BGFX_STENCIL_FUNC_REF(stencilValue)
+                    | BGFX_STENCIL_FUNC_RMASK(0xff)
+                    | BGFX_STENCIL_OP_FAIL_S_REPLACE
+                    | BGFX_STENCIL_OP_FAIL_Z_REPLACE
+                    | BGFX_STENCIL_OP_PASS_Z_REPLACE,
+                BGFX_STENCIL_NONE,
                 BGFX_DISCARD_ALL);
+
+        for(auto& segment : model.m_Model->GetMesh()->GetSegments())
+        {
+            debug_draw::setWireframe(true);
+            {
+                bx::Vec3 centre =
+                {
+                    segment.m_Bounds.m_Sphere.center.x + positionRaw.m_Position.x(),
+                    segment.m_Bounds.m_Sphere.center.y + positionRaw.m_Position.y(),
+                    segment.m_Bounds.m_Sphere.center.z + positionRaw.m_Position.z(),
+                };
+
+                const bx::Sphere sphere = { { centre }, segment.m_Bounds.m_Sphere.radius };
+                debug_draw::createScope();
+                debug_draw::setColor(0x44ffc0f0_argb);
+                debug_draw::setLod(0);
+                debug_draw::draw(sphere);
+            }
+        }
     }
 }
