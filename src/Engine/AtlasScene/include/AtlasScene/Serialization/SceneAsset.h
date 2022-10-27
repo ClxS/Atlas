04046ca5ce4f6de6d9ff42f0b3fc61aa7e692ddc
Carl <slxxls92@gmail.com>
2022-10-27 19:07:12 +0100

    Scene asset stub

---
+++ src/Engine/AtlasScene/include/AtlasScene/Serialization/SceneAsset.h
@@ -0,0 +1,15 @@
+﻿#pragma once
+#include "AtlasResource/AssetPtr.h"
+#include "AtlasResource/FileData.h"
+#include "AtlasResource/ResourceAsset.h"
+
+namespace atlas::scene
+{
+    class SceneAsset
+    {
+    public:
+
+    };
+
+    resource::AssetPtr<resource::ResourceAsset> sceneLoadHandler(const resource::FileData& data);
+}
---
+++ src/Engine/AtlasScene/src/Serialization/SceneAsset.cpp
@@ -0,0 +1,7 @@
+﻿#include "AtlasScenePCH.h"
+#include "SceneAsset.h"
+
+atlas::resource::AssetPtr<atlas::resource::ResourceAsset> atlas::scene::sceneLoadHandler(const resource::FileData& data)
+{
+    return nullptr;
+}
